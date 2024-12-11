7e02295ff1eb6ed5f51626511735f63bd4f3a9e5
valid <janipatrik138@gmail.com>
2024-12-11 16:52:04 +0100

    fix

--- src/pages/landing/TV.vue
+++ src/pages/landing/TV.vue
@@ -11,7 +11,7 @@
       <ul class="title-container">
         <li v-for="(song, index) in songs" :key="song.songId">
           <span :style="{ backgroundColor: colors[index] }" class="color-box"></span>
-          <span class="song-title">{{ song.songTitle }}</span>
+          <span class="song-title">{{ song.songTitle }} - {{ song.voteCount }}</span>
         </li>
       </ul>
     </div>
@@ -27,6 +27,19 @@ Chart.register(ChartDataLabels);
 export default {
   setup() {
     const songs = ref([
+      { songId: "e33d8a9e-506b-4423-be15-4bccbca941fe", songTitle: "Alma20", voteCount: 121 },
+      { songId: "9a380d38-3537-4b4d-8e8e-4dafb9fad375", songTitle: "zene1", voteCount: 110 },
+      { songId: "a5962c85-09de-4115-a7d5-7a8811ac25f6", songTitle: "zene2", voteCount: 140 },
+      { songId: "b2fbe141-f3f1-46a7-b3af-16d6a0c0baf9", songTitle: "alma", voteCount: 160 },
+      { songId: "d09a3d33-780f-40a1-a591-83e87e31dc5f", songTitle: "title", voteCount: 120 },
+      { songId: "969bf7e2-d5f0-49a5-856c-cab9d92900dc", songTitle: "asdawgagsgfghjavwfdhkagbkfluaa", voteCount: 11 },
+      { songId: "e33d8a9e-506b-4423-be15-4bccbca941fe", songTitle: "Alma20", voteCount: 121 },
+      { songId: "9a380d38-3537-4b4d-8e8e-4dafb9fad375", songTitle: "zene1", voteCount: 110 },
+      { songId: "a5962c85-09de-4115-a7d5-7a8811ac25f6", songTitle: "zene2", voteCount: 140 },
+      { songId: "b2fbe141-f3f1-46a7-b3af-16d6a0c0baf9", songTitle: "alma", voteCount: 160 },
+      { songId: "d09a3d33-780f-40a1-a591-83e87e31dc5f", songTitle: "title", voteCount: 120 },
+      { songId: "d09a3d33-780f-40a1-a591-83e87e31dc5f", songTitle: "title", voteCount: 120 },
+      { songId: "969bf7e2-d5f0-49a5-856c-cab9d92900dc", songTitle: "asdawgagsgfghjavwfdhkagbkfluaa", voteCount: 11 },
       { songId: "e33d8a9e-506b-4423-be15-4bccbca941fe", songTitle: "Alma20", voteCount: 121 },
       { songId: "9a380d38-3537-4b4d-8e8e-4dafb9fad375", songTitle: "zene1", voteCount: 110 },
       { songId: "a5962c85-09de-4115-a7d5-7a8811ac25f6", songTitle: "zene2", voteCount: 140 },
@@ -99,7 +112,7 @@ export default {
             datalabels: {
               color: 'black',
               font: {
-                size: 30,
+                size: 20,
               },
               anchor: 'end',
               align: 'top',
